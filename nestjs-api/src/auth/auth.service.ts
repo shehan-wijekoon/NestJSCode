2dde23a7dd4f7ab67655464ea96ce724bf1cbc5d
shehan-wijekoon <shehan.wijekoon@gmail.com>
2026-01-14 11:26:57 +0530

    Refactor user and bookmark models, update auth logic
    Renamed database tables to 'Users' and 'Bookmarks' and updated Prisma schema accordingly. Added unique constraint to user email and established relation between bookmarks and users. Enhanced auth service to handle signup/signin with proper error handling and credential verification. Updated controller to accept AuthDto for signin.

--- nestjs-api/src/auth/auth.service.ts
+++ nestjs-api/src/auth/auth.service.ts
@@ -1,31 +1,57 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/client";
 
 
 @Injectable({})
 export class AuthService {
-    constructor(private prisma : PrismaService){}
-    async signup(dto:AuthDto) {
+    constructor(private prisma: PrismaService) { }
+    async signup(dto: AuthDto) {
         //generate the password
         const hash = await argon.hash(dto.password);
         //save the user in the database
-        const user = await this.prisma.user.create({
-            data: {
-                email: dto.email,
-                hash,
-            },
-        });
-        //return the user
-        return user;
-    
+
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                },
+            });
+
+            //return the user
+            return user;
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Credentials taken')
+                }
+
+            }
+
+            throw error;
+        }
+
     }
 
-    signin() {
-        return {
-            msg: 'i am signin'
-        }
+    async signin(dto: AuthDto) {
+
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email,
+            },
+        });
+        if (!user) throw new ForbiddenException('Credentials incorrect');
+
+        const pwMatches = await argon.verify(user.hash, dto.password);
+
+        if (!pwMatches) throw new ForbiddenException('Credentials incorrect');
+
+        return user;
+
     }
 
 }
